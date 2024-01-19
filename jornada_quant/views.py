64a0e9da561ef102394901af6f7c2998aaa5b330
Luis Fernando Giglio <luisfgiglio@gmail.com>
2024-01-19 17:55:45 -0300

    Busca no fundamentus so empresas cadastradas

--- jornada_quant/views.py
+++ jornada_quant/views.py
@@ -2,11 +2,13 @@ import fundamentus
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+from datetime import datetime, timedelta
 
 from django.shortcuts import render
 from django.views.generic import View
 
-from datetime import datetime, timedelta
+from core.models import Companies
+
 
 # Create your views here.
 
@@ -14,14 +16,11 @@ class IndexView(View):
     template_name = "quant_index.html"
 
     def get(self, request, *args, **kwargs):
+        list_companies = Companies.objects.filter(is_active=True).values_list('slug', flat=True)
         df = fundamentus.get_resultado()
         df['papel'] = df.index.str[:4]
-        df_agrupado = df.drop_duplicates(subset=["papel"]).round(2)
-        
-        # TODO Criar tabela para armazenar nome ticker se esta ativa ou nao das empresas Filtrar a tabelas pelas astivas
-
-        # df_agrupado['traded_recently'] = df.index.map(lambda papel: self.get_traded_recently(papel))
-        # df_filter = df_agrupado[df_agrupado['traded_recently']].round(2)
+        df_filtrado = df[df.index.isin(list_companies)].round(2)
+        df_agrupado = df_filtrado.drop_duplicates(subset=["papel"])
         data = df_agrupado.to_dict(orient='index')
         return render(request, self.template_name, {'content':data})
 
