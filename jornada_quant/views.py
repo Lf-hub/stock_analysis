54cdc6450df60fad415a903df77cbce6ba4ecd85
Luis Fernando Giglio <luisfgiglio@gmail.com>
2024-01-18 22:48:08 -0300

    WIP

--- jornada_quant/views.py
+++ jornada_quant/views.py
@@ -6,6 +6,8 @@ import yfinance as yf
 from django.shortcuts import render
 from django.views.generic import View
 
+from datetime import datetime, timedelta
+
 # Create your views here.
 
 class IndexView(View):
@@ -14,8 +16,21 @@ class IndexView(View):
     def get(self, request, *args, **kwargs):
         df = fundamentus.get_resultado()
         df['papel'] = df.index.str[:4]
-        df_agrupado = df.drop_duplicates(subset=["papel"])
-        # TODO criar funcao onde verifica as ações que ainda estao ativas
-        df_agrupado = df_agrupado.round(2)
+        df_agrupado = df.drop_duplicates(subset=["papel"]).round(2)
+        # TODO Chamar função para verificar se todos os tickes estao ativos na B3
+        # df_agrupado['traded_recently'] = df.index.map(lambda papel: self.get_traded_recently(papel))
+        # df_filter = df_agrupado[df_agrupado['traded_recently']].round(2)
         data = df_agrupado.to_dict(orient='index')
         return render(request, self.template_name, {'content':data})
+
+    def get_traded_recently(self, papel):
+        try:
+            ticker = papel+'.SA'
+            stock = yf.Ticker(ticker)
+            historical_data = stock.history(period='1mo')
+            last_day = historical_data.index[-1].date()
+            today = datetime.now().date()
+            start = today - timedelta(days=5)
+            return start <= last_day <= today
+        except Exception:
+            return False
